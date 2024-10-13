27767b599061a43b95bd9c98388f5b65a0436917
MoisesCALEB <145431319+MoisesCALEB@users.noreply.github.com>
2024-10-13 12:28:31 +0200

    creation

---
+++ foodies/src/test/java/com/tfg/app/foodies/FoodiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.app.foodies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
