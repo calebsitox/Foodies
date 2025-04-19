6ce41709c287479597e6ab1ac3e072bff0ebf1ee
calebsitox <127038749+calebsitox@users.noreply.github.com>
2025-04-19 09:34:23 +0200

    Adding Like Button

--- AndroidFoodies/app/src/main/java/com/aula/androidfoodies/ui/theme/LocationSearchScreen.kt
+++ AndroidFoodies/app/src/main/java/com/aula/androidfoodies/ui/theme/LocationSearchScreen.kt
@@ -13,17 +13,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -151,6 +156,19 @@ fun LocationSearchScreen(
                                 text = place["address"] ?: "Sin dirección",
                                 style = MaterialTheme.typography.bodyMedium
                             )
+
+                            Spacer(modifier = Modifier.height(8.dp))
+
+                            IconButton(
+                                onClick = {},
+                                modifier =  Modifier.align(Alignment.End)
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Favorite,
+                                    contentDescription = "Like",
+                                    tint = Color.Red
+                                )
+                            }
                         }
                     }
                 }
