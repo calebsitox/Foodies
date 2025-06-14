b45189b28dbbec8cfee35c74ec05bb3a3235014c
calebsitox <127038749+calebsitox@users.noreply.github.com>
2025-06-14 13:20:28 +0200

    Adding NGROK

--- AndroidFoodies/app/src/main/java/com/aula/androidfoodies/retrofit/RetrofitInstance.kt
+++ AndroidFoodies/app/src/main/java/com/aula/androidfoodies/retrofit/RetrofitInstance.kt
@@ -10,7 +10,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 
 object RetrofitInstance {
 
-    private const val BASE_URL = "http://10.0.2.2:8081/"
+    private const val BASE_URL = "https://16e8-79-116-247-191.ngrok-free.app/"
         //"https://11e1-213-94-59-186.ngrok-free.app "
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
